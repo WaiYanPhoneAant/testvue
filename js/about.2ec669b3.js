2694fc580a348bf00df1a83efcd4e9fb60d23c17
WaiYanPhoneAant <wypa07@gmail.com>
2023-01-04 17:00:14 +0630

    Deploy

---
+++ js/about.2ec669b3.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkedit_app"]=self["webpackChunkedit_app"]||[]).push([[443],{242:function(n,t,a){a.r(t),a.d(t,{default:function(){return d}});var e=a(252);const u={class:"about"},r=(0,e._)("h1",null,"This is an about page",-1),s=[r];function i(n,t){return(0,e.wg)(),(0,e.iD)("div",u,s)}var c=a(744);const p={},o=(0,c.Z)(p,[["render",i]]);var d=o}}]);
+//# sourceMappingURL=about.2ec669b3.js.map
